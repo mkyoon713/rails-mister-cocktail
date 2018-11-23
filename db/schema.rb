e5e96901e639a9e4a5d95d160283d321404b942f
Minkyeong Yoon <mkyoon713@gmail.com>
2018-11-23 17:02:22 +0900

    created cocktail app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_022719) do
+ActiveRecord::Schema.define(version: 2018_11_23_072632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2018_11_22_022719) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
+    t.text "description"
   end
 
   create_table "doses", force: :cascade do |t|
